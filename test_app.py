87dd7a9ac4a9e7fc66c30e00ec12ece32ee7f2aa
Dylan Vogel <boomxsf@gmail.com>
2024-04-24 13:09:40 -0700

    Adding weather pyfiles

--- test_app.py
+++ test_app.py
@@ -1,11 +1,13 @@
 import pytest
 from app import app  # Import the app object from app.py
 
+
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
 
 def test_get_weather_data(client):
     rv = client.get('/weather')
